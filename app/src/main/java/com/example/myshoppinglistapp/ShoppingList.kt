43843be7ed7638d8ed37e6b8107a12c6713fe53e
mustafa_macbook <mustafaanis247@gmail.com>
2024-05-29 18:23:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -0,0 +1,188 @@
+package com.example.myshoppinglistapp
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+data class ShopingList(
+    val id:Int,
+    var name:String,
+    var quantity:Int,
+    var isEditing: Boolean =false
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShopingListApp(){
+    var sItems by remember {
+        mutableStateOf(listOf<ShopingList>())
+    }
+//    val sItems = remember {
+//        mutableStateListOf<ShopingList>()
+//    }
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
+    var itemName by remember {
+        mutableStateOf("")
+    }
+    var itemQuantity by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = { showDialog = true},
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Add Item")
+
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(sItems){
+                ShoppingListItem(it,{},{})
+
+            }
+
+        }
+    }
+
+    if (showDialog){
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            confirmButton = {
+                            Row (
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ){
+                                Button(
+                                    onClick = {
+                                        if(itemName.isNotBlank()){
+                                            val newItem = ShopingList(
+                                                id = sItems.size + 1,
+                                                name = itemName,
+                                                quantity = itemQuantity.toInt()
+                                            )
+                                            sItems = sItems + newItem
+                                            showDialog = false
+                                            itemName = ""
+                                        }
+
+
+
+                                    }) {
+                                    Text(text = "Add")
+                                }
+                                Button(
+                                    onClick = {showDialog = false}) {
+                                    Text(text = "Cancel")
+                                }
+
+                            }
+            },
+            title = { Text(text = "Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange ={itemName = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                        )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange ={itemQuantity = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+
+            }
+
+
+        )
+
+    }
+
+}
+@Composable
+fun ShoppingListItem(
+    item:ShopingList,
+    onEditClick: ()->Unit,
+    onDeleteClick: ()->Unit
+){
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color.Blue),
+                shape = RoundedCornerShape(20)
+            )
+    ){
+        
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+
+        Row (modifier = Modifier.padding(8.dp)){
+
+            IconButton(onClick = {onEditClick}) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+                
+            }
+
+        }
+
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun ShopingListItemPreview(){
+    val myItem = ShopingList(1,"Computer",4)
+    ShoppingListItem(myItem,{},{})
+}
